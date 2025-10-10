6635b3c53747b6579bca04b20a0ef11df9c857c4
0x0226 <113506290+0x0226@users.noreply.github.com>
2025-10-10 14:55:18 +0800

    Update Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -136,6 +136,11 @@ const Sidebar = ({ onToggle, activePath = '/' }: SidebarProps) => {
       href: '/douban?type=tv',
     },
     {
+      icon: PlaySquare,
+      label: '短剧',
+      href: '/shortdrama',
+    },
+    {
       icon: Cat,
       label: '动漫',
       href: '/douban?type=anime',
@@ -144,11 +149,6 @@ const Sidebar = ({ onToggle, activePath = '/' }: SidebarProps) => {
       icon: Clover,
       label: '综艺',
       href: '/douban?type=show',
-    },
-    {
-      icon: PlaySquare,
-      label: '短剧',
-      href: '/shortdrama',
     },
     {
       icon: Radio,
