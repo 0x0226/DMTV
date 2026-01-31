ce06a278550b7609459dd12883d4ce0cacd301b5
0x0226 <113506290+0x0226@users.noreply.github.com>
2026-01-31 20:36:55 +0800

    Update UserMenu.tsx

--- src/components/UserMenu.tsx
+++ src/components/UserMenu.tsx
@@ -248,7 +248,7 @@ export const UserMenu: React.FC = () => {
 
       const savedDoubanDataSource = localStorage.getItem('doubanDataSource');
       const defaultDoubanProxyType =
-        (window as any).RUNTIME_CONFIG?.DOUBAN_PROXY_TYPE || 'direct';
+        (window as any).RUNTIME_CONFIG?.DOUBAN_PROXY_TYPE || 'cors-proxy-zwei';
       if (savedDoubanDataSource !== null) {
         setDoubanDataSource(savedDoubanDataSource);
       } else if (defaultDoubanProxyType) {
